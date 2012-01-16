62a6f1a189152e038ef668bdc801a7fc36a5e2c7
Rafa de Castro <rafael@micubiculo.com>
2012-01-16 08:04:10 +0100

    Stops in case it is not possible to load sdl-mixer

--- rythmic.rb
+++ rythmic.rb
@@ -1,3 +1,10 @@
 require "rubygems"
 
 require "lib/drumkit"
+
+if defined?(Rubygame::Sound).nil?
+  puts "Couldn't load SDL and SDL_Mixer. Are they correcly installed?. Please check https://github.com/rafadc/rythmic for installation instructions."
+  exit
+end
+
+
