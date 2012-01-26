a7dd3bf9eddf415fec818c186b923879a7d6be4f
Rafa de Castro <rafael@micubiculo.com>
2012-01-26 22:33:38 +0100

    Added stop and changed thread handling

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,20 +1,29 @@
 class Sample
   SAMPLES_FOLDER = "samples"
-  
+
   def initialize(sample_name)
     @sound = Rubygame::Sound.load("#{SAMPLES_FOLDER}/#{sample_name}")
     @delays = [0]
   end
-  
+
+  def play_sync
+    play
+    @thread.join
+  end
+
   def play
-    Thread.new do
+    @thread = Thread.new do
       @delays.each do |delay|
         sleep delay
         @sound.play
       end
     end
   end
-  
+
+  def stop
+    @thread.kill
+  end
+
   def play_after(*seconds)
     @delays = seconds
   end
