ad70e72f67ce1e7a904a3f24abeba2ebb4cb5ce1
Rafa de Castro <rafael@micubiculo.com>
2012-01-29 00:38:09 +0100

    Amending example

--- examples/drumkit_example.rb
+++ examples/drumkit_example.rb
@@ -1,7 +1,7 @@
 require "./rhythmic"
 
 drums = Rhythmic::Drumkit.new "gmkit"
-drums.pattern("kick", 0, 0, 1)
-drums.pattern("tom_hi", 0, 2)
+drums.pattern("kick", 0, [0, 1])
+drums.pattern("tom_hi", 0, [2])
 drums.play
 
