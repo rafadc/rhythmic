66762e5b1014a095d96f8d81e447110f29a43255
Rafa de Castro <rafael@micubiculo.com>
2012-01-29 02:00:56 +0100

    Made drum kit example work with new patter specification

--- examples/drumkit_example.rb
+++ examples/drumkit_example.rb
@@ -3,5 +3,8 @@ require "./rhythmic"
 drums = Rhythmic::Drumkit.new "gmkit"
 drums.pattern("kick", 0, [0, 1])
 drums.pattern("tom_hi", 0, [2])
+drums.copy_pattern(0, 1)
+drums.copy_pattern(0, 2)
+drums.copy_pattern(0, 3)
 drums.play
 
