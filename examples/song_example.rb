d4acc85aec277163cb7ce20257c45c4e63758239
Rafa de Castro <rafael@micubiculo.com>
2012-01-29 01:59:37 +0100

    Given a better song example

--- examples/song_example.rb
+++ examples/song_example.rb
@@ -1,7 +1,28 @@
 require "./rhythmic"
 
-drums = Rhythmic::Drumkit.new "gmkit"
-drums.pattern("kick", 1, [0, 1, 3, 5, 6])
-drums.pattern("tom_hi", 1, [0, 2])
-drums.play
+demo_song = Rhythmic::Song.new
+
+drums = Rhythmic::Drumkit.new("gmkit", 230, 10)
+drums.pattern("clap", 0, [0, 2, 4, 6, 8, 10])
+drums.copy_pattern(0, 1)
+drums.copy_pattern(0, 2)
+drums.copy_pattern(0, 3)
+drums.copy_pattern(0, 4)
+drums.copy_pattern(0, 5)
+demo_song.add drums
+
+your_in = Rhythmic::Sample.new("YourInOrYourOut.wav")
+your_in.volume = 0.7
+your_in.play_after(0, 4, 4, 4, 4, 4)
+demo_song.add your_in
+
+your_smokin = Rhythmic::Sample.new("YourSmokin.wav")
+your_smokin.play_after(10, 4)
+demo_song.add your_smokin
+
+thank_you = Rhythmic::Sample.new("IJustWannaThankYou.wav")
+thank_you.play_after(18)
+demo_song.add thank_you
+
+demo_song.play
 
